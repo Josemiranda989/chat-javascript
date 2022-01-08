5764ffdff4ebb7ce186c9ad4e0e218dcb1857039
José Miranda <josemiranda989@gmail.com>
2022-01-08 17:37:57 -0300

    Mensajes y usuarios listos

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,20 +1,51 @@
 $(function () {
+  const socket = io();
 
-    const socket = io();
+  /* Obtaining DOM elements from the interface */
+  const $messageForm = $("#message-form");
+  const $messageBox = $("#message");
+  const $chat = $("#chat");
 
-    /* Obtaining DOM elements from the interface */
-    const $messageForm = $('#message-form')
-    const $messageBox = $("#message");
-    const $chat = $("#chat");
+  /* Obtaining DOM elements from the nicknameForm */
+  const $nickForm = $("#nickForm");
+  const $nickname = $("#nickname");
+  const $nickError = $("#nickError");
+    const $users = $("#usernames");
+    
+    $nickForm.submit(e => {
+        e.preventDefault()
+        socket.emit('new user', $nickname.val(), data => {
+            if (data) {
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            } else {
+                $nickError.html(`
+                <div class='alert-danger'>
+                That username already exits.
+                </div>
+                `);
+            }
+            $nickname.val('');
+        })
 
-    /* Events */
-    $messageForm.submit(e => {
-        e.preventDefault();
-        socket.emit('send message', $messageBox.val())
-        $messageBox.val('')
     })
 
-    socket.on('new message', function (data) {
-        $chat.append(data + "<br/>")
+  /* Events */
+  $messageForm.submit((e) => {
+    e.preventDefault();
+    socket.emit("send message", $messageBox.val());
+    $messageBox.val("");
+  });
+
+  socket.on("new message", function (data) {
+      $chat.append("<b>" + data.nick + "</b>: "+ data.msg + "<br/>");
+  });
+   
+    socket.on('usernames', data => {
+        let html = '';
+        for (let i = 0; i < data.length; i++) {
+            html += `<p><i class='fas fa-user'></i> ${data[i]}</p>`
+        }
+        $users.html(html);
     })
-})
+});
